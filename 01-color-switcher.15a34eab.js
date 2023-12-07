ca352283c934a4d98b9912ed5ee1b1701acd68d0
Bezobrazz <103434668+Bezobrazz@users.noreply.github.com>
2023-12-07 12:35:41 +0000

    Deploying to gh-pages from @ Bezobrazz/goit-js-hw-09@056eae5b87cad66d6a3692741a275a90cf45aea9 🚀

---
+++ 01-color-switcher.15a34eab.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=document.body;e.addEventListener("click",(function(){t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(t),a.disabled=!0,e.disabled=!1})),a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.15a34eab.js.map
